f753c24f3c741dbcb25ef0a0dacbb7cc57e59dec
Jelagat2640 <sugutasumpta@gmail.com>
2026-05-19 09:02:42 +0300

    login page

--- over-drive-frontend/src/router/AppRouter.jsx
+++ over-drive-frontend/src/router/AppRouter.jsx
@@ -1,18 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-
-import MainLayout from "../layouts/MainLayout";
 import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <Login />,
-      },
-    ],
+    element: <Login />,
   },
 ]);
 
